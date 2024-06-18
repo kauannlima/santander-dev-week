902811753d33f6c3cfe84106d5baf16ea6584b69
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-06-18 00:02:40 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2023Application.class, args);
+	}
+
+}
